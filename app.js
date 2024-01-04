5247c4855156813756d7ff1bdb6d3b6a1c13d4a7
Max Shpungin <maxsdance@gmail.com>
2024-01-03 16:14:00 -0800

    got post route working!

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 "use strict";
 
 const express = require("express");
-
 const db = require("./fakeDb");
+
+const itemRoutes = require('./itemRoutes')
+
+const app = express()
+//we need JSON
+app.use(express.json());
+
+app.use("/items", itemRoutes)
+
+/** 404 handler: matches unmatched routes; raises NotFoundError. */
+app.use(function (req, res) {
+  throw new NotFoundError();
+});
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
+module.exports = app;
--- itemRoutes.js
+++ itemRoutes.js
@@ -7,39 +7,47 @@ const router = new express.Router();
 
 const { items } = require("./fakeDb.js");
 
-/** Return list of shopping items */
+/** GET /items:  Return list of shopping items */
 
-router.get("/", function(req, res){
-  return res.json({"items": items});
+router.get("/", function (req, res) {
+  return res.json({ items });
 });
 
+//TODO: item class?
 
-/** Accept JSON body, add item, and return it */
+/** POST /items: Accept JSON body, add item, and return it */
 
-router.post("/", function(req, res){
+router.post("/", function (req, res) {
   const newItem = req.body.item;
+  //debugger;
   items.push(newItem);
-  return newItem;
+  return res.send({ added: newItem });
 });
 
 
-/** Return single item from URL parameter */
+/** GET /items/:name: Return single item from URL parameter */
 
-router.get("/:name", function(req, res){
+router.get("/:name", function (req, res) {
+  //look through our items array for an item with name :name
+  // return that item
+ // const name = req.params
+  for(item of items){
+    if(item.name)
+  }
 
 });
 
 
 /** Accept JSON body, modify item, return it  */
 
-router.patch("/:name", function(req, res){
+router.patch("/:name", function (req, res) {
 
 });
 
 
 /** Delete item given in URL parameter */
 
-router.delete("/:name", function(req, res){
+router.delete("/:name", function (req, res) {
 
 });
 
